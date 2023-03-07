48bab19c6ebb826a70b3069c311dd86dba8e8e1c
CofCat456 <a25690756@gmail.com>
2023-03-08 00:58:07 +0800

    chore: 🐙 install eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,11 +1,33 @@
 module.exports = {
   root: true,
+  env: {
+    browser: true,
+    es2021: true
+  },
   extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
+    'airbnb-base',
     '@vue/eslint-config-prettier/skip-formatting'
   ],
+  overrides: [],
   parserOptions: {
-    ecmaVersion: 'latest'
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  settings: {
+    'import/resolver': {
+      alias: {
+        map: [['@', './src']],
+        extensions: ['.js', '.vue']
+      }
+    },
+    'import/core-modules': ['vite', '@vitejs/plugin-vue']
+  },
+  plugins: ['vue'],
+  rules: {
+    semi: ['error', 'always'],
+    'no-console': 'off',
+    'vue/multi-word-component-names': 'off'
   }
 };
