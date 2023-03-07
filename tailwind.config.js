dd86cc8642e04e88048ce50f8cf369b4c1e698c4
CofCat456 <a25690756@gmail.com>
2023-03-08 01:32:11 +0800

    docs: 🐜 define base config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx,vue}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        'cc-primary': '#0F4BB4',
+        'cc-accent': '#EE5220',
+        'cc-other-1': '#FFFFFF',
+        'cc-other-2': '#181818',
+        'cc-other-3': '#666666',
+        'cc-other-4': '#9F9F9F',
+        'cc-other-5': '#D4D4D4',
+        'cc-other-6': '#E9E9E9',
+        'cc-other-7': '#F7F7F7',
+        'cc-other-8': '#181818/80'
+      }
+    }
   },
   plugins: []
 };
