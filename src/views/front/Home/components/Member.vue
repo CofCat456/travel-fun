faa1847605f6771a16c4b5a504e11ed44c0abc28
CofCat456 <a25690756@gmail.com>
2023-03-10 02:20:39 +0800

    feat: 🐈 add home member

---
+++ src/views/front/Home/components/Member.vue
@@ -0,0 +1,22 @@
+<script setup></script>
+
+<template>
+  <div
+    class="relative flex h-[600px] w-full bg-cover bg-center bg-no-repeat"
+    style="
+      background: linear-gradient(0deg, rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)),
+        url(/images/member.png);
+    "
+  >
+    <div
+      class="absolute top-1/2 left-1/2 w-full max-w-[636px] -translate-x-1/2 -translate-y-1/2 bg-cc-other-8 py-6 px-10 text-center text-white"
+    >
+      <h4>帶著感激與期待，讓我們踏上旅程！</h4>
+      <span class="material-icons-outlined my-10" style="font-size: 40px"> escalator_warning </span>
+      <p class="mb-4 text-sm leading-5">
+        當人們去旅行時，他們不僅僅是去看風景，更是去體驗不同的文化和風土人情，開拓視野和擴大人脈，旅行可以帶給人們豐富的體驗和回憶
+      </p>
+      <button type="button" class="btn">成為會員</button>
+    </div>
+  </div>
+</template>
