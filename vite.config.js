cedda95a4bbc43ce944d538a30c70f2db0f26636
CofCat456 <a25690756@gmail.com>
2023-03-10 08:18:36 +0800

    docs: 🐜 localhost server

--- vite.config.js
+++ vite.config.js
@@ -15,5 +15,10 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  server: {
+    open: true,
+    port: 3000,
+    host: '0.0.0.0'
   }
 });
